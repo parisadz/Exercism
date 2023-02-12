ef9ed3223046af8b127d2dadac57f15b5eedf024
Parisa <81490901+parisadz@users.noreply.github.com>
2023-02-12 21:34:23 +0330

    Resistor Color for step5

---
+++ Resistor-Color.js
@@ -0,0 +1,23 @@
+export const colorCode = (color) => {
+  return COLORS.indexOf(color);
+  // if(color === "orange"){
+  //   return 3;
+  // }
+  // if(color === "white"){
+  //   return 9;
+  // }
+  // return 0;
+};
+
+export const COLORS = [
+  "black",
+  "brown",
+  "red",
+  "orange",
+  "yellow",
+  "green",
+  "blue",
+  "violet",
+  "grey",
+  "white",
+];
